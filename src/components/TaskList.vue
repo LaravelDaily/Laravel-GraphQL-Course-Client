366eae5c98d30b2ddd9256f13925c64e5c7a7cfa
PovilasKorop <povilask007@gmail.com>
2021-11-15 13:16:09 +0200

    First GraphQL Request

---
+++ src/components/TaskList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="task in tasks" :key="task.id">{{ task.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import TASKS_QUERY from "@/graphql/Tasks.gql";
+export default {
+  data() {
+    return {
+      tasks: null,
+    };
+  },
+  apollo: {
+    tasks: TASKS_QUERY,
+  },
+};
+</script>
