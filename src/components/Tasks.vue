ac2a546c00ae8fdd02f7d4666cb17fc293f9ef73
PovilasKorop <povilask007@gmail.com>
2021-11-17 11:07:09 +0200

    Delete task

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     <ul>
-      <li v-for="task in tasks" :key="task.id">{{ task.title }}</li>
+      <li v-for="task in tasks" :key="task.id">
+        {{ task.title }}
+        <button @click="delete_task(task.id)">Delete</button>
+      </li>
     </ul>
     <button @click="open_form = true">Add a task</button>
 
@@ -15,6 +18,7 @@
 <script>
 import TASKS_QUERY from "@/graphql/Tasks.gql";
 import CREATE_TASK_MUTATION from "@/graphql/CreateTask.gql";
+import DELETE_TASK_MUTATION from "@/graphql/DeleteTask.gql";
 
 export default {
   data() {
@@ -34,6 +38,16 @@ export default {
       }).then(() => {
         this.title = ''
         this.open_form = false
+        this.$apollo.queries.tasks.refetch()
+      })
+    },
+    delete_task(id) {
+      this.$apollo.mutate({
+        mutation: DELETE_TASK_MUTATION,
+        variables: {
+          id: id
+        }
+      }).then(() => {
         this.$apollo.queries.tasks.refetch()
       })
     }
