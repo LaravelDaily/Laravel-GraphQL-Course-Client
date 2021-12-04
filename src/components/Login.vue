9cd1086ad02e676df7bf8dcf0b19c630547b2276
PovilasKorop <povilask007@gmail.com>
2021-12-04 10:45:18 +0200

    Login form - work in progress: Sanctum call

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,42 @@
 <template>
-  <div>
-    Login form coming soon.
-  </div>
+    <form @submit.prevent="submitLogin">
+      Email
+      <br/>
+      <input v-model="email" type="email"/>
+      <br/><br/>
+      Password
+      <br/>
+      <input v-model="password" type="password"/>
+      <br/><br/>
+      <button type="submit">Log In</button>
+    </form>
 </template>
 
+<script>
+import axios from "axios";
+
+export default {
+  name: "Login",
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    submitLogin() {
+      axios
+          .post(process.env.VUE_APP_SERVER_HTTP + "/api/sanctum/token", {
+            email: this.email,
+            password: this.password,
+            device_name: navigator.userAgent,
+          })
+          .then(async (response) => {
+            console.log("Token: " + response.data);
+            this.$router.push("/");
+          })
+          .catch((error) => console.log(error));
+    },
+  },
+}
+</script>
